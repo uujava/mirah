285925b7610fcc95922f21c9e967bd67c5d91faa
uujava <uujava@gmail.com>
2016-02-22 23:21:52 +0300

    revert felixvf fix 417

--- test/fixtures/org/foo/XXTypeFixtureJava8.java
+++ test/fixtures/org/foo/XXTypeFixtureJava8.java
@@ -3,7 +3,7 @@ package org.foo;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
-public class xx_type_fixture {
+public class XXTypeFixtureJava8 {
     public CompletableFuture<List<Runnable>> load(List filter, int flags){
         return null;
     }
